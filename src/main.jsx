f4da3c48275eb63ceb7c75c02fcfefff3157039d
ElenaKhlebnikova <elenano555@gmail.com>
2023-06-22 12:05:56 +0200

    changed workflow

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-console.log('mm');
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
